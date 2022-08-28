dd74a2c08268723b9b3c789c9e90062a4d06c94c
jikor1st <jikor1st@naver.com>
2022-08-28 20:08:54 +0900

    feat: project init settings

---
+++ README.old.md
@@ -0,0 +1,2 @@
+# jigi-blog-thumbnail-generator
+저의 블로그의 썸네일 이미지를 생성하는 앱입니다.
---
+++ src/lib/modules/namedLazy.ts
@@ -0,0 +1,31 @@
+import { lazy } from "react";
+
+const NamedLazy = <T extends {}, U extends keyof T>(
+  loader: (x?: string) => Promise<T>,
+  timeout: number | undefined = 0
+) => {
+  return new Proxy({} as unknown as T, {
+    get: (target, componentName: string | symbol, receiver) => {
+      if (typeof componentName === "string") {
+        return lazy(() =>
+          loader(componentName).then(async (x) => {
+            const componentObject = {
+              default: x[componentName as U] as any as React.ComponentType<any>,
+            };
+            if (timeout) {
+              return await new Promise<{ default: React.ComponentType }>(
+                (res, rej) => {
+                  setTimeout(() => res(componentObject), timeout);
+                }
+              );
+            } else {
+              return componentObject;
+            }
+          })
+        );
+      }
+    },
+  });
+};
+
+export { NamedLazy };
